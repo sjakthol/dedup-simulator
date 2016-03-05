09d462d8c3399b82c166e7f88db0c67e045d346f
Sami Jaktholm <sjakthol@outlook.com>
2016-03-05 16:00:23 +0200

    Improve the output of perfect simulator
    It now prints both file and byte counts.

--- simulator/simulator-perfect.py
+++ simulator/simulator-perfect.py
@@ -23,34 +23,56 @@ def simulate():
     # A set of files already in the storage
     seen = set()
 
-    # The size of the all uploads combined (deduplicated or not)
-    total_in = 0
-
-    # The size of the data sent to the service
-    data_in = 0
+    files_uploaded = 0
+    data_uploaded = 0
+    files_in_storage = 0
+    data_in_storage = 0
 
     tmr = timer.Timer()
+
+    def print_stats():
+        data = (
+            utils.num_fmt(files_in_storage),
+            utils.num_fmt(files_uploaded),
+            1 - files_in_storage / files_uploaded,
+            utils.sizeof_fmt(data_in_storage),
+            utils.sizeof_fmt(data_uploaded),
+            1 - data_in_storage / data_uploaded,
+            utils.get_mem_info(),
+            tmr.elapsed_str
+        )
+
+        tmpl = (
+            "Statistics: \n"
+            "  Files: files_in_storage=%s, files_uploaded=%s, DDP=%s\n"
+            "  Data: data_in_storage=%s, data_uploaded=%s, DDP=%s\n"
+            "  Execution: memory[%s], chunk_time=%s"
+        )
+
+        tmr.reset()
+
+        print(tmpl % data, file=sys.stderr)
+
     for (i, (hsh, size)) in enumerate(utils.read_upload_stream()):
-        total_in += size
+        files_uploaded += 1
+        data_uploaded += size
         if hsh not in seen:
-            data_in += size
+            files_in_storage += 1
+            data_in_storage += size
             seen.add(hsh)
 
         if (i + 1) % utils.REPORT_FREQUENCY == 0:
-            print("%s uploads, percentage %.4f, time %s, mem[%s]" % (
-                utils.num_fmt(i),
-                1 - data_in / total_in,
-                tmr.elapsed_str,
-                utils.get_mem_info()
-            ), file=sys.stderr)
+            print_stats()
 
-        print("%i,%i" % (data_in, total_in))
+        print("%i,%i,%i,%i" % (
+            files_in_storage,
+            files_uploaded,
+            data_in_storage,
+            data_uploaded,
+        ))
 
-    dedup_percentage = 1 - data_in / total_in
-    print("+++ Done. stored=%s, uploaded=%s, dedup_percentage=%f" % (
-        utils.sizeof_fmt(data_in),
-        utils.sizeof_fmt(total_in),
-        dedup_percentage), file=sys.stderr)
+    print("+++ Done; ", end="", file=sys.stderr)
+    print_stats()
 
 if __name__ == "__main__":
     simulate()
